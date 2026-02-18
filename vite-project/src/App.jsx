0cadb2a052221c3519af3216b1476abd294ed3e8
Asya Haroyan <alharoyan@gmail.com>
2026-02-18 15:03:40 +0100

    start the app

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import MovieCard from './components/MovieCard'
+
+function App() {
+  return (
+    <>
+   <MovieCard movie={{title: "Tim's film", realease_date: "2024"}}/>
+    </>
+  )
+}
+
+export default App
